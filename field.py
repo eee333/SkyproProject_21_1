717b5a4beddb5c14cf1bfab21570e8595481b603
Igor <91094458+ipotemkin@users.noreply.github.com>
2021-12-23 23:21:03 +0300

    Изменения в field.py
    1. Код в фунциях move_unit_up, move_unit_dow, move_unit_left, move_unit_right повторяется почти один в один. Можно изменять координаты в начале метода, а дельнейший код вынести во вспомогательный метод.
    2. Методы get_rows и get_cols не нужны

--- field.py
+++ field.py
@@ -6,44 +6,31 @@ class Field:
     def cell(self, coord):  # метод, возвращающий объект находящийся по данным координатам;
         x, y = coord
         return self.field_array[y][x]
-
-    def move_unit_up(self):  # метод, смещающий юнита вверх;
-        x, y = self.unit.get_coordinates()
-        step_on_obj = self.cell((x, y - 1)).get_obj()
+    
+    def _do_move(self, x, y):
+       step_on_obj = self.cell((x, y)).get_obj()
         step_on_obj.step_on(self.unit)
         if step_on_obj.is_walkable():
-            self.unit.set_coordinates((x, y - 1))
+            self.unit.set_coordinates((x, y))
+    
+    def move_unit_up(self):  # метод, смещающий юнита вверх;
+        x, y = self.unit.get_coordinates()
+        self._do_move(x, y-1)
 
     def move_unit_down(self):  # метод, смещающий юнита вниз;
         x, y = self.unit.get_coordinates()
-        step_on_obj = self.cell((x, y + 1)).get_obj()
-        step_on_obj.step_on(self.unit)
-        if step_on_obj.is_walkable():
-            self.unit.set_coordinates((x, y + 1))
+        self._do_move(x, y+1)
 
     def move_unit_right(self):  # метод, смещающий юнита вправо;
         x, y = self.unit.get_coordinates()
-        step_on_obj = self.cell((x + 1, y)).get_obj()
-        step_on_obj.step_on(self.unit)
-        if step_on_obj.is_walkable():
-            self.unit.set_coordinates((x + 1, y))
+        self._do_move(x+1, y)
 
     def move_unit_left(self):  # метод, смещающий юнита влево;
         x, y = self.unit.get_coordinates()
-        step_on_obj = self.cell((x - 1, y)).get_obj()
-        step_on_obj.step_on(self.unit)
-        if step_on_obj.is_walkable():
-            self.unit.set_coordinates((x - 1, y))
-
+        self._do_move(x-1, y)
 
     def get_field(self):  # возвращает свойство field.
         return self.field_array
-
-    def get_cols(self):  # возвращает количество столбцов в поле.
-        pass
-
-    def get_rows(self):  # возвращает количество строк в поле
-        pass
 
 
 class Cell:
